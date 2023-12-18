43d2ea14774d8fa46fc2c1ecc262881e56e287f6
Kevin.P <kevin.pellissard@gmail.com>
2023-12-18 21:16:51 +0100

    Update pycodestyle

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ import csv
 if __name__ == '__main__':
     todos = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos',
-        params ={"_expand" : "user"})
+        params={"_expand": "user"})
     username = todos.json()[0]['user']['username']
     with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
