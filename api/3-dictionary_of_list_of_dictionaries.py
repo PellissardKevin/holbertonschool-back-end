4c91a876dffb7011ce49d618ba3b460fb37cf7cf
Kevin.P <kevin.pellissard@gmail.com>
2023-12-20 15:40:35 +0100

    Update task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""Script that save in csv infos from a given employee"""
-
+"""Script to use a REST API, returns information about
+all tasks from all employees and export in JSON"""
 import json
 import requests
 
-API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    users = requests.get(f"https://jsonplaceholder.typicode.com/users").json()
 
-    users = requests.get(f'{API_URL}/users').json()
-    todos = requests.get(f"{API_URL}/todos").json()
+    dict_users_tasks = {}
+    for user in users:
+        tasks = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{user['id']}/todos"
+            ).json()
 
-    json_filename = "todo_all_employees.json"
+        dict_users_tasks[user["id"]] = []
+        for task in tasks:
+            task_dict = {
+                "username": user["username"],
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            dict_users_tasks[user["id"]].append(task_dict)
 
-    data = {}
-
-    for task in todos:
-        user_id = task['userId']
-        if user_id not in data:
-            data[user_id] = []
-        data[user_id].append({
-            "username": next(user['username']
-                             for user in users
-                             if user['id'] == user_id),
-            "task": task['title'],
-            "completed": task['completed']
-        })
-    with open(json_filename, 'w') as file:
-        json.dump(data, file)
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(dict_users_tasks, file)
