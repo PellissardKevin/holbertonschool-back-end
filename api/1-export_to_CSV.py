14a3a556e27c9d91946495f3f07fc8d5c40d6e86
Kevin.P <kevin.pellissard@gmail.com>
2023-12-20 15:24:16 +0100

    Update task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""API to csv format"""
+"""Script that save in csv infos from a given employee"""
 
+import csv
 import requests
 from sys import argv
-import csv
 
 API_URL = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
+
     USER_ID = argv[1]
+
     user = requests.get(f'{API_URL}/users/{USER_ID}').json()
     todo_list = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
 
-    with open(f"{USER_ID}.csv", mode='w') as csv_file:
+    csv_filename = f"{USER_ID}.csv"
+
+    with open(csv_filename, mode='w') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         for task in todo_list:
