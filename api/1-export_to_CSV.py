fcefbff5e3986b6bf2a695da31a2ca26fbd089c8
Kevin.P <kevin.pellissard@gmail.com>
2023-12-20 15:15:47 +0100

    Update task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """API to csv format"""
+
 import requests
 from sys import argv
 import csv
 
+API_URL = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
-    todos = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos',
-        params={"_expand": "user"})
+    todos = requests.get(f'{API_URL}/users/{argv[1]}/todos',
+                         arams={"_expand": "user"})
     username = todos.json()[0]['user']['username']
     with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
