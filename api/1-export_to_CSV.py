82c77a1217158dbb46b804a1857b29262dadde99
Kevin.P <kevin.pellissard@gmail.com>
2023-12-20 15:06:40 +0100

    Update task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,18 +5,13 @@ from sys import argv
 import csv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos",
-        params={"_expand": "user"},
-    )
-    username = todos.json()[0]["user"]["username"]
-    with open("{}.csv".format(argv[1]), "w", newline="") as f:
+        f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos',
+        params={"_expand": "user"})
+    username = todos.json()[0]['user']['username']
+    with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for todo in todos.json():
-            csv_writer.writerow(
-                [user["id"],
-                user["username"],
-                task["completed"],
-                task["title"]]
-            )
+            writer.writerow(
+                [argv[1], username, str(todo['completed']), todo['title']])
