63f38c1f84cc1c9ae46194c54371550db11f3c6e
Kevin.P <kevin.pellissard@gmail.com>
2023-12-21 14:57:37 +0100

    Update task 3 add a condition for create dictionnary

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -16,12 +16,13 @@ if __name__ == "__main__":
 
         dict_users_tasks[user["id"]] = []
         for task in tasks:
-            task_dict = {
-                "username": user["username"],
-                "task": task["title"],
-                "completed": task["completed"]
-            }
-            dict_users_tasks[user["id"]].append(task_dict)
+            if task['userId'] == user['id']:
+                task_dict = {
+                    "username": user["username"],
+                    "task": task["title"],
+                    "completed": task["completed"]
+                }
+                dict_users_tasks[user["id"]].append(task_dict)
 
     with open("todo_all_employees.json", "w") as file:
         json.dump(dict_users_tasks, file)
